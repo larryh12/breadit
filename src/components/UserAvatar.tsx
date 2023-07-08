bc52d1ad8bd2d9148f7520c66cc624bc9c3455a3
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-07-08 14:05:01 +0700

    feat: add user avatar comp

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,33 @@
+import { User } from "next-auth";
+import { FC } from "react";
+import { Avatar, AvatarFallback } from "./ui/Avatar";
+import Image from "next/image";
+import { Icons } from "./Icons";
+
+interface UserAvatarProps {
+  user: Pick<User, "name" | "image">;
+}
+
+const UserAvatar: FC<UserAvatarProps> = ({ user }) => {
+  return (
+    <Avatar>
+      {user.image ? (
+        <div className="relative aspect-square h-full w-full">
+          <Image
+            fill
+            src={user.image}
+            alt="user profile picture"
+            referrerPolicy="no-referrer"
+          />
+        </div>
+      ) : (
+        <AvatarFallback>
+          <span className="sr-only">{user?.name}</span>
+          <Icons.user className="h-4 w-4" />
+        </AvatarFallback>
+      )}
+    </Avatar>
+  );
+};
+
+export default UserAvatar;
