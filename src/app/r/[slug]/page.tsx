ba75c08671de182959e187bb75e638b46450bf83
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-07-09 17:50:29 +0700

    feat: render subreddit page

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import MiniCreatePost from "@/components/MiniCreatePost";
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
+
+const page = async ({
+  params: { slug },
+}: {
+  params: {
+    slug: string;
+  };
+}) => {
+  const session = await getAuthSession();
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: INFINITE_SCROLL_PAGINATION_RESULTS,
+      },
+    },
+  });
+  if (!subreddit) return notFound();
+
+  return (
+    <>
+      <h1 className="font-bold text-3xl md:text-4xl h-14">
+        r/{subreddit.name}
+      </h1>
+      <MiniCreatePost session={session} />
+      {/* <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} /> */}
+    </>
+  );
+};
+
+export default page;
