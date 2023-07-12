dd8edea3f597d065b18cdea6ec06badcb52ab8e8
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-07-12 16:00:45 +0700

    feat: comment reply

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -32,7 +32,6 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
       author: true,
       votes: true,
       replies: {
-        // first level replies
         include: {
           author: true,
           votes: true,
@@ -46,6 +45,65 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
       <hr className="w-full h-px my-6" />
 
       <CreateComment postId={postId} />
+
+      <div className="flex flex-col gap-y-6 mt-4">
+        {comments
+          .sort((a, b) => b.votes.length - a.votes.length) // Sort by most liked
+          .map((topLevelComment) => {
+            const topLevelCommentVotesAmt = topLevelComment.votes.reduce(
+              (acc, vote) => {
+                if (vote.type === "UP") return acc + 1;
+                if (vote.type === "DOWN") return acc - 1;
+                return acc;
+              },
+              0
+            );
+            const topLevelCommentVote = topLevelComment.votes.find(
+              (vote) => vote.userId === session?.user.id
+            );
+
+            return (
+              <div key={topLevelComment.id} className="flex flex-col">
+                <div className="mb-2">
+                  <PostComment
+                    comment={topLevelComment}
+                    currentVote={topLevelCommentVote}
+                    votesAmt={topLevelCommentVotesAmt}
+                    postId={postId}
+                  />
+                </div>
+
+                {/* Render replies */}
+                {topLevelComment.replies
+                  .sort((a, b) => b.votes.length - a.votes.length) // Sort by most liked
+                  .map((reply) => {
+                    const replyVotesAmt = reply.votes.reduce((acc, vote) => {
+                      if (vote.type === "UP") return acc + 1;
+                      if (vote.type === "DOWN") return acc - 1;
+                      return acc;
+                    }, 0);
+                    const replyVote = reply.votes.find(
+                      (vote) => vote.userId === session?.user.id
+                    );
+
+                    return (
+                      <div
+                        key={reply.id}
+                        className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
+                      >
+                        <PostComment
+                          comment={reply}
+                          currentVote={replyVote}
+                          votesAmt={replyVotesAmt}
+                          postId={postId}
+                        />
+                      </div>
+                    );
+                  })}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
